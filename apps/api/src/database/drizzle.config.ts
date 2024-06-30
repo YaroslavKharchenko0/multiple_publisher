fd6af9ac7519809b72e2d9249a813fd03d0599ec
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-06-30 16:28:59 +0300

    feat: add database

---
+++ apps/api/src/database/drizzle.config.ts
@@ -0,0 +1,14 @@
+import { resolve } from "path"
+
+const schemaPath = resolve(__dirname, "drizzle.schema.ts")
+
+const migrationsPath = resolve(__dirname, "../../migrations")
+
+export default {
+  dialect: "postgresql",
+  schema: schemaPath,
+  out: migrationsPath,
+  dbCredentials: {
+    url: process.env.DATABASE_URL,
+  }
+}
