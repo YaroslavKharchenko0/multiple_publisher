24cc5e6a9fef4bf56e76e17bc9713e7e9390ea04
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-07-01 14:29:04 +0300

    fix: remove auth

--- infra/external/terraform/main.tf
+++ infra/external/terraform/main.tf
@@ -1,24 +1,5 @@
 terraform {
-  required_providers {
-    keycloak = {
-      source  = "mrparkers/keycloak"
-      version = "4.4.0"
-    }
-    local = {
-      source  = "hashicorp/local"
-      version = "2.1.0"
-    }
-  }
+
 }
 
 
-module "auth" {
-  source = "./modules/auth"
-
-  keycloak_url           = var.keycloak_url
-  keycloak_admin_username = var.keycloak_admin_username
-  keycloak_admin_password = var.keycloak_admin_password
-  keycloak_service_name  = var.keycloak_service_name
-  env                    = var.env
-  valid_redirect_uris = var.valid_redirect_uris
-}
