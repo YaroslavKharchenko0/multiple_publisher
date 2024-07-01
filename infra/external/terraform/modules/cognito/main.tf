a319ba1e649be5a02a9ef726f727c11caefba674
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-07-01 19:20:14 +0300

    feat: update cognito config

--- infra/external/terraform/modules/cognito/main.tf
+++ infra/external/terraform/modules/cognito/main.tf
@@ -1,11 +1,20 @@
 resource "aws_cognito_user_pool" "user_pool" {
   name = var.user_pool_name
 
+  auto_verified_attributes = ["email"]
+
   schema {
     attribute_data_type = "String"
     name                = "role"
     mutable             = true
     required            = false
+  }
+
+  verification_message_template {
+    default_email_option  = "CONFIRM_WITH_CODE"
+    email_message         = "Your verification code is {####}"
+    email_message_by_link = "Click the link below to verify your email address: {##Verify Email##}"
+    email_subject         = "Verify your email for our app"
   }
 }
 
