a0256d4c73cb2df233c788401bdc527b6a282b9e
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-07-02 22:25:13 +0300

    feat: add subscription on event

--- apps/api/src/modules/user-roles/controllers/event.controller.ts
+++ apps/api/src/modules/user-roles/controllers/event.controller.ts
@@ -1,18 +1,24 @@
-import { EventEvent } from "@app/contracts";
+import { UserCreatedEvent } from "@app/contracts";
 import { RabbitPayload, RabbitSubscribe } from "@golevelup/nestjs-rabbitmq";
-import { Controller } from "@nestjs/common";
+import { Controller, Inject } from "@nestjs/common";
+import { USER_ROLE_SERVICE } from "../providers/user-role.providers";
+import { UserRoleService } from "../services/user-role.service";
+import { Role } from "@app/types";
 
 @Controller()
 export class EventController {
-  constructor() { }
+  constructor(@Inject(USER_ROLE_SERVICE) private readonly userRoleService: UserRoleService) { }
 
   @RabbitSubscribe({
-    exchange: EventEvent.exchange,
-    routingKey: EventEvent.routingKey,
-    queue: EventEvent.queue,
+    exchange: UserCreatedEvent.exchange,
+    routingKey: UserCreatedEvent.routingKey,
+    queue: UserCreatedEvent.queue,
   })
-  event(@RabbitPayload() message: EventEvent.Request): void {
-    console.log('Event', message);
+  async onCreateUser(@RabbitPayload() message: UserCreatedEvent.Request) {
+    await this.userRoleService.createUserRoleByRoleName({
+      role: Role.USER,
+      userId: message.id
+    })
   }
 }
 
