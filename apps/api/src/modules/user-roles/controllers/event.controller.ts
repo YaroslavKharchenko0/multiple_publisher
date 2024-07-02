9a0cb9d6fccc835c47277b01fcd8592d563eff0a
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-07-02 22:40:26 +0300

    fix: tracing

--- apps/api/src/modules/user-roles/controllers/event.controller.ts
+++ apps/api/src/modules/user-roles/controllers/event.controller.ts
@@ -4,6 +4,7 @@ import { Controller, Inject } from "@nestjs/common";
 import { USER_ROLE_SERVICE } from "../providers/user-role.providers";
 import { UserRoleService } from "../services/user-role.service";
 import { Role } from "@app/types";
+import { TraceId } from "@app/logger";
 
 @Controller()
 export class EventController {
@@ -14,11 +15,11 @@ export class EventController {
     routingKey: UserCreatedEvent.routingKey,
     queue: UserCreatedEvent.queue,
   })
-  async onCreateUser(@RabbitPayload() message: UserCreatedEvent.Request) {
+  async onCreateUser(@RabbitPayload() message: UserCreatedEvent.Request, @TraceId() traceId: string) {
     await this.userRoleService.createUserRoleByRoleName({
       role: Role.USER,
       userId: message.id
-    })
+    }, { traceId })
   }
 }
 
