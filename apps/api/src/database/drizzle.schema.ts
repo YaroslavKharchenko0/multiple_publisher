d01120bc354ed9655df1aa85cef953436490d4e1
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-07-02 19:45:16 +0300

    feat: update migrations & add base roles

--- apps/api/src/database/drizzle.schema.ts
+++ apps/api/src/database/drizzle.schema.ts
@@ -1,9 +1,34 @@
-import { serial, text, timestamp, pgTable, uuid } from "drizzle-orm/pg-core";
+import { relations } from "drizzle-orm";
+import { serial, text, timestamp, pgTable, uuid, varchar, integer } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
-  id: serial("id"),
+  id: serial("id").primaryKey(),
   email: text("email").unique().notNull(),
   providerId: uuid("provider_id").unique().notNull(),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
 });
+
+export const roles = pgTable("roles", {
+  id: serial("id").primaryKey(),
+  role: varchar("role", { length: 50 }).unique().notNull(),
+});
+
+export const userRoles = pgTable("user_roles", {
+  id: serial("id").primaryKey(),
+  userId: integer("user_id").notNull().references(() => users.id, { onDelete: 'cascade' }),
+  roleId: integer("role_id").notNull().references(() => roles.id, { onDelete: 'cascade' }),
+});
+
+export const usersRelations = relations(users, ({ many }) => ({
+  userRoles: many(userRoles),
+}));
+
+export const rolesRelations = relations(roles, ({ many }) => ({
+  userRoles: many(userRoles),
+}));
+
+export const userRolesRelations = relations(userRoles, ({ one }) => ({
+  user: one(users),
+  role: one(roles),
+}));
---
+++ apps/api/src/modules/roles/controllers/api.controller.ts
@@ -0,0 +1,71 @@
+import { AmqpConnection } from "@golevelup/nestjs-rabbitmq";
+import { Controller, Get } from "@nestjs/common";
+import { CommandCommand, CommandErrorCommand, EventEvent, QueryQuery } from '@app/contracts'
+import { TraceId } from "@app/logger";
+
+@Controller('roles')
+
+export class ApiController {
+  constructor(private readonly amqpConnection: AmqpConnection) { }
+
+  @Get('/command')
+  command(@TraceId() traceId: string | undefined) {
+    const payload: CommandCommand.Request = {
+      message: 'Hello World'
+    }
+
+    return this.amqpConnection.request<CommandCommand.Response>({
+      exchange: CommandCommand.exchange,
+      routingKey: CommandCommand.routingKey,
+      payload,
+      headers: {
+        traceId
+      }
+    });
+  }
+
+  @Get('/event')
+  event(@TraceId() traceId: string | undefined) {
+    const payload: EventEvent.Request = {
+      message: 'Hello World'
+    }
+
+    return this.amqpConnection.publish<EventEvent.Request>(EventEvent.exchange, EventEvent.routingKey, payload, {
+      headers: {
+        traceId
+      }
+    });
+  }
+
+  @Get('/query')
+  query(@TraceId() traceId: string | undefined) {
+    const payload: QueryQuery.Request = {
+      message: 'Hello World'
+    }
+
+    return this.amqpConnection.request<QueryQuery.Response>({
+      exchange: QueryQuery.exchange,
+      routingKey: QueryQuery.routingKey,
+      payload,
+      headers: {
+        traceId
+      }
+    });
+  }
+
+  @Get('/error')
+  error(@TraceId() traceId: string | undefined) {
+    const payload: CommandErrorCommand.Request = {
+      message: 'Hello World'
+    }
+
+    return this.amqpConnection.request<CommandErrorCommand.Response>({
+      exchange: CommandErrorCommand.exchange,
+      routingKey: CommandErrorCommand.routingKey,
+      payload,
+      headers: {
+        traceId
+      }
+    });
+  }
+}
