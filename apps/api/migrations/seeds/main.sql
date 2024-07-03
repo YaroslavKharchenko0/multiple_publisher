1b2003b29bd1df193fbde4d1e3d7b397a9ad6062
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-07-04 00:49:31 +0300

    feat: update seed file

---
+++ apps/api/migrations/seeds/main.sql
@@ -0,0 +1,20 @@
+INSERT INTO roles (role) VALUES ('user'), ('admin') ON CONFLICT DO NOTHING;
+
+CREATE OR REPLACE FUNCTION check_workspace_deletion()
+  RETURNS TRIGGER AS $$
+  BEGIN
+    IF (
+      SELECT COUNT(*)
+      FROM workspaces
+      WHERE user_id = OLD.user_id
+    ) = 1 THEN
+      RAISE EXCEPTION 'Cannot delete the last workspace of a user';
+    END IF;
+    RETURN OLD;
+  END;
+  $$ LANGUAGE plpgsql;
+
+CREATE TRIGGER before_workspace_delete
+  BEFORE DELETE ON workspaces
+  FOR EACH ROW
+  EXECUTE FUNCTION check_workspace_deletion();
