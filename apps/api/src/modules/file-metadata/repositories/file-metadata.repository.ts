c08bf0c6ac6a2c37ef5970bf1ed5a343c2e5d98c
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-07-05 22:10:56 +0300

    feat: add file metadata repo

---
+++ apps/api/src/modules/file-metadata/repositories/file-metadata.repository.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@nestjs/common";
+import { Database, Orm, schema } from "../../../database";
+import { eq } from "drizzle-orm";
+import { Pagination } from "@app/validation";
+
+export type InsertFileMetadata = typeof schema.fileMetadata.$inferInsert
+export type SelectFileMetadata = typeof schema.fileMetadata.$inferSelect
+
+@Injectable()
+export class FileMetadataRepository {
+  constructor(@Orm() private readonly db: Database) { }
+
+  private fileMetadata = schema.fileMetadata;
+
+  async createOne(input: InsertFileMetadata) {
+    return this.db.insert(this.fileMetadata).values(input).returning().execute();
+  }
+
+  async findByFile(id: number, pagination: Pagination) {
+    const where = eq(this.fileMetadata.id, id);
+
+    const result = await this.db.query.fileMetadata.findMany({
+      where,
+      ...pagination,
+    })
+
+    return result;
+  }
+
+  async deleteById(id: number) {
+    const where = eq(this.fileMetadata.id, id);
+
+    const result = await this.db.delete(this.fileMetadata).where(where).returning().execute();
+
+    return result;
+  }
+}
