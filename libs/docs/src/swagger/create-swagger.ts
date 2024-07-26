a830f6675ef43fbae29ebc506e5812067d6389a8
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-07-26 19:17:15 +0300

    feat: add swagger

---
+++ libs/docs/src/swagger/create-swagger.ts
@@ -0,0 +1,30 @@
+import { ConfigService } from '@nestjs/config';
+import { NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { RawServerDefault } from 'fastify';
+import { patchNestJsSwagger } from 'nestjs-zod';
+
+export const createSwagger = async (
+  app: NestFastifyApplication<RawServerDefault>,
+) => {
+  const builder = new DocumentBuilder();
+
+  const configService = app.get(ConfigService);
+
+  builder.setTitle('API');
+  builder.setDescription('API description');
+
+  const appVersion = configService.getOrThrow<string>('VERSION');
+
+  builder.setVersion(appVersion);
+
+  const config = builder.build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('swagger', app, document, {
+    jsonDocumentUrl: 'swagger/json',
+  });
+
+  patchNestJsSwagger();
+};
