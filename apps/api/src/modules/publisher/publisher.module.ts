487a282d0ca65400fbdb3c3703267a975ae5d8b8
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-08-03 14:35:37 +0300

    feat: init publisher module

---
+++ apps/api/src/modules/publisher/publisher.module.ts
@@ -0,0 +1,15 @@
+import { RmqModule } from '@app/rmq';
+import { DynamicModule, Module } from '@nestjs/common';
+import { ApiController } from './controllers/api.controller';
+import { CommandController } from './controllers/command.controller';
+
+@Module({})
+export class PublisherModule {
+  static forRoot(): DynamicModule {
+    return {
+      module: PublisherModule,
+      imports: [RmqModule.forRoot()],
+      controllers: [ApiController, CommandController],
+    };
+  }
+}
