45fb03d53ceafe70f68299c8d99cb066323b68a4
YaroslavKharchenko0 <yaroslav.harchenko@voypost.com>
2024-10-16 22:43:21 +0300

    feat: add docs

--- libs/docs/src/swagger/modules/auth.ts
+++ libs/docs/src/swagger/modules/auth.ts
@@ -1,12 +1,17 @@
 import { applyDecorators } from '@nestjs/common';
 import { defaultDecorators } from './default';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiBody, ApiOperation } from '@nestjs/swagger';
+import { SignInBodyDto } from '@app/dtos';
 
 export const SignUpDocs = () =>
   applyDecorators(...defaultDecorators, ApiOperation({ summary: 'Sign up' }));
 
 export const SignInDocs = () =>
-  applyDecorators(...defaultDecorators, ApiOperation({ summary: 'Sign in' }));
+  applyDecorators(
+    ...defaultDecorators,
+    ApiOperation({ summary: 'Sign in' }),
+    ApiBody({ type: SignInBodyDto }),
+  );
 
 export const VerifyEmailDocs = () =>
   applyDecorators(
--- libs/dtos/src/auth/sign-in.dto.ts
+++ libs/dtos/src/auth/sign-in.dto.ts
@@ -1,4 +1,11 @@
 import { signInValidationSchema } from '@app/validation';
-import { createZodDto } from '@anatine/zod-nestjs'
+import { createZodDto } from '@anatine/zod-nestjs';
 
-export class SignInBodyDto extends createZodDto(signInValidationSchema) { }
+import { extendApi } from '@anatine/zod-openapi';
+
+const withDocs = extendApi(signInValidationSchema, {
+  title: 'SignInBody',
+  example: { email: 'some@gmail.com', password: 'Password' },
+});
+
+export class SignInBodyDto extends createZodDto(withDocs) { }
